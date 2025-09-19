9bbf64234321cb2ef773cd7b8c720f234d83ac04
miguelmml12 <miguelmedinamml016@gmail.com>
2025-09-19 12:15:58 -0500

    Creacion de Clases generales
    Se crearon las clases que van a ser utilizadas y se agrego parcialmete cierta implementacion

---
+++ SistemaDeArriendo/src/main/java/com/mycompany/sistemadearriendo/Manager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemadearriendo;
+
+import java.util;
+/**
+ *
+ * @author angel
+ */
+public class Manager {
+    private String nombre;
+    private String cedula;
+    
+    
+    
+}
---
+++ SistemaDeArriendo/src/main/java/com/mycompany/sistemadearriendo/SistemaDeArriendo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadearriendo;
+
+/**
+ *
+ * @author angel
+ */
+public class SistemaDeArriendo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
